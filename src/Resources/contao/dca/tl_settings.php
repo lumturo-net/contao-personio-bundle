f87d5406ae76435427f52997e5273713c8cb16b2
Rolf Staege <rolf.staege@lumturo.net>
2021-05-12 10:24:41 +0200

    Parameter-fix

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -6,8 +6,13 @@ $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] .= ';{personio_legend},
  * Add fields
  */
 $GLOBALS['TL_DCA']['tl_settings']['fields']['personio_webservice_url'] = array(
-    'label' => &$GLOBALS['TL_LANG']['tl_settings']['personio_webservice_url'],
-    'exclude' => TRUE,
+    'label'     => &$GLOBALS['TL_LANG']['tl_settings']['personio_webservice_url'],
+    'exclude'   => true,
     'inputType' => 'text',
-    'eval' => array('mandatory' => TRUE)
+    'eval'      => array(
+        'mandatory'      => true,
+        // 'allowHtml'      => false,
+        // 'decodeEntities' => false,
+        'useRawRequestData' => true // sonst wird das = entity_decoded
+    ),
 );
